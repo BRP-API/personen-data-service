d6151b8c9a1597a0610e3cca7ae70ed98c1c3d51
Melvin Lee <melvin.lee@iswish.nl>
2024-07-16 16:34:32 +0200

    ci: docker compose up ipv build gebruikt om lokaal container image creatie te forceren

--- scripts/containers-build.sh
+++ scripts/containers-build.sh
@@ -3,7 +3,9 @@
 MODE=$1
 
 if [ "$MODE" = "ci" ]; then
-    docker compose -f .docker/docker-compose-ci.yml build
+    # gebruik docker compose up om te forceren dat de container image wordt aangemaakt in de lokale registry
+    docker compose -f .docker/docker-compose-ci.yml up
+    docker compose -f .docker/docker-compose-ci.yml down
 else
     docker compose -f src/docker-compose.yml build
 fi
