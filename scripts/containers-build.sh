ffbbbb5073c8bbf9cdb667d5cf0c2c26f8717e94
Melvin Lee <melvin.lee@iswish.nl>
2024-07-16 16:47:05 +0200

    ci: containers opstarten in detached mode, zodat ze weer kunnen worden gestopt

--- scripts/containers-build.sh
+++ scripts/containers-build.sh
@@ -3,8 +3,8 @@
 MODE=$1
 
 if [ "$MODE" = "ci" ]; then
-    # gebruik docker compose up om te forceren dat de container image wordt aangemaakt in de lokale registry
-    docker compose -f .docker/docker-compose-ci.yml up
+    # gebruik docker compose up -d om te forceren dat de container image wordt aangemaakt in de lokale registry
+    docker compose -f .docker/docker-compose-ci.yml up -d
     docker compose -f .docker/docker-compose-ci.yml down
 else
     docker compose -f src/docker-compose.yml build
