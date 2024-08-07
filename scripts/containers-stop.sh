1ff33a3138929b2a7605efcac786ce8ac1491d3e
Melvin Lee <melvin.lee@iswish.nl>
2024-08-07 13:34:56 +0200

    ci: data-service validatie stappen toegevoegd

--- scripts/containers-stop.sh
+++ scripts/containers-stop.sh
@@ -3,13 +3,13 @@
 MODE=$1
 
 if [ "$MODE" = "ci" ]; then
-    docker compose -f .docker/db-ci.yml down
+    docker compose -f .docker/db-ci.yml down --volumes
 else
-    docker compose -f .docker/db.yml down
+    docker compose -f .docker/db.yml down --volumes
 fi
 
 docker compose \
     -f .docker/gezag-mock.yml \
     -f .docker/gezag-proxy-mock.yml \
     -f .docker/personen-data-service.yml \
-    down
+    down --volumes
