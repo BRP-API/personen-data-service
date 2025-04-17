8f486274b74cbc2ec4261eb20309d9bcbc52bf7f
Melvin Lee <melvin.lee@iswish.nl>
2025-04-17 08:35:33 +0200

    chore: missende aanpassingen voor expressieve stap definities toegevoegd

--- scripts/generate-step-summary.js
+++ scripts/generate-step-summary.js
@@ -1,34 +1,20 @@
 const fs = require('fs');
 const path = require('path');
+const { processFile } = require('./process-cucumber-file');
 
-const inputFile = path.join(__dirname, './../test-reports/cucumber-js/personen/test-result-summary.txt');
 const outputFile = path.join(__dirname, './../test-reports/cucumber-js/personen/step-summary.txt');
+fs.writeFileSync(outputFile, '', 'utf8');
 
-function processFile(inputPath, outputPath) {
-    const content = fs.readFileSync(inputPath, 'utf8');
-    const lines = content.split('\n');
-    const results = [];
-    let footerLines = [];
+const fileMap = new Map([
+    ["./../test-reports/cucumber-js/step-definitions/test-result-zonder-dependency-integratie-summary.txt", "docs (zonder integratie)"],
+    ["./../test-reports/cucumber-js/step-definitions/test-result-integratie-summary.txt", "docs (integratie)"],
+    ["./../test-reports/cucumber-js/step-definitions/test-result-informatie-api-summary.txt", "docs informatie api context"],
+    ["./../test-reports/cucumber-js/step-definitions/test-result-data-api-summary.txt", "docs data api context"],
+    ["./../test-reports/cucumber-js/step-definitions/test-result-gezag-api-summary.txt", "docs gezag api context"],
+    ["./../test-reports/cucumber-js/step-definitions/test-result-gezag-api-deprecated-summary.txt", "docs gezag api deprecated context"],
+    ["./../test-reports/cucumber-js/personen/test-result-summary.txt", "personen"]
+]);
 
-    if (lines.length >= 4) {
-        footerLines = lines.slice(-4);
-    }
-
-    lines.forEach((line) => {
-        const match = line.match(/^\d+\) Scenario: .*# (.+:\d+)/);
-        if (match) {
-            results.push((results.length + 1) + ": " + match[1]);
-        }
-    });
-
-    let finalOutput = footerLines.join('\n') + '\n\n';
-    if (results.length > 0) {
-        finalOutput += `<details>\n<summary>Bestandsnamen en regelnummers</summary>\n\n`;
-        finalOutput += results.join('\n') + '\n';
-        finalOutput += `\n</details>\n`;
-    }
-
-    fs.writeFileSync(outputPath, finalOutput, 'utf8');
-}
-
-processFile(inputFile, outputFile);
+fileMap.forEach((caption, filePath) => {
+    processFile(path.join(__dirname, filePath), outputFile, caption);
+});
