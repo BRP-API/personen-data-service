f935bff07f7fbb359b3b053ef2dcbb09c6b2b2cb
Melvin Lee <melvin.lee@iswish.nl>
2025-06-04 15:42:54 +0200

    chore: gbaNaamPersoon zonder additionalProperties

--- src/Rvig.HaalCentraalApi.Personen/ApiModels/BRP/Deprecated/Generated.cs
+++ src/Rvig.HaalCentraalApi.Personen/ApiModels/BRP/Deprecated/Generated.cs
@@ -490,15 +490,6 @@ namespace Rvig.HaalCentraalApi.Personen.ApiModels.BRP.Deprecated
         [Newtonsoft.Json.JsonProperty("aanduidingNaamgebruik", Required = Newtonsoft.Json.Required.Default, NullValueHandling = Newtonsoft.Json.NullValueHandling.Ignore)]
         public Waardetabel AanduidingNaamgebruik { get; set; }
 
-        private System.Collections.Generic.IDictionary<string, object> _additionalProperties;
-
-        [Newtonsoft.Json.JsonExtensionData]
-        public System.Collections.Generic.IDictionary<string, object> AdditionalProperties
-        {
-            get { return _additionalProperties ?? (_additionalProperties = new System.Collections.Generic.Dictionary<string, object>()); }
-            set { _additionalProperties = value; }
-        }
-
     }
 
     [System.CodeDom.Compiler.GeneratedCode("NJsonSchema", "14.4.0.0 (NJsonSchema v11.3.2.0 (Newtonsoft.Json v13.0.0.0))")]
