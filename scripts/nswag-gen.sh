52f4b873de0441cfff3232e2e59f51372b037838
nkorporaal <nap.korporaal+github@gmail.com>
2025-06-04 13:48:31 +0200

    chore: genereer deprecated en actuele dtos

--- scripts/nswag-gen.sh
+++ scripts/nswag-gen.sh
@@ -1,8 +1,11 @@
 #!/bin/bash
 
 sed -i '' 's/^\([[:space:]]*\)\(properties\)/\1additionalProperties: false\n&/g' ./specificatie/resolved/openapi.yaml
+sed -i '' 's/^\([[:space:]]*\)\(properties\)/\1additionalProperties: false\n&/g' ./specificatie/resolved/openapi-v1.yaml
 
 npx nswag run src/GezagMock/server.nswag
 npx nswag run src/DataTransferObjects.nswag
+npx nswag run src/DataTransferObjectsDeprecated.nswag
 
 git checkout -- ./specificatie/resolved/openapi.yaml
+git checkout -- ./specificatie/resolved/openapi-v1.yaml
