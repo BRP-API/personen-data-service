6e0ce54a80c89b22b39784ae9defe5577dd67706
nkorporaal <nap.korporaal+github@gmail.com>
2025-06-06 08:49:30 +0200

    fix controller

--- src/Rvig.HaalCentraalApi.Personen/Controllers/GbaApiPersonenController.cs
+++ src/Rvig.HaalCentraalApi.Personen/Controllers/GbaApiPersonenController.cs
@@ -10,77 +10,54 @@ namespace Rvig.HaalCentraalApi.Personen.Controllers;
 [ApiController, Route("haalcentraal/api/brp"), ValidateContentTypeHeader]
 public class GbaApiPersonenController : GbaApiBaseController
 {
-	private readonly IGbaPersonenApiService _gbaService;
-	private readonly IGezagService _gezagService;
-    private readonly GezagService _newGezagService;
+    private readonly IGbaPersonenApiService _gbaService;
+    private readonly IGezagService _gezagService;
 
-    public GbaApiPersonenController(IGbaPersonenApiService gbaService, IGezagService gezagService, GezagService newGezagService)
-	{
-		_gbaService = gbaService;
-		_gezagService = gezagService;
-		_newGezagService = newGezagService;
-	}
+    public GbaApiPersonenController(IGbaPersonenApiService gbaService, IGezagService gezagService)
+    {
+        _gbaService = gbaService;
+        _gezagService = gezagService;
+    }
 
 
-	[HttpPost]
-	[Route("personen")]
-	public async Task<IActionResult> GetPersonen([FromBody] PersonenQuery model)
-	{
-		await ValidateUnusableQueryParams(model);
+    [HttpPost]
+    [Route("personen")]
+    public async Task<IActionResult> GetPersonen([FromBody] PersonenQuery model)
+    {
+        await ValidateUnusableQueryParams(model);
 
-		var vraagtBsn = model.Fields.Contains("burgerservicenummer");
+        var vraagtBsn = model.Fields.Contains("burgerservicenummer");
 
-		(PersonenQueryResponse personenResponse, List<long>? plIds, List<string>? bsns) = await _gbaService.GetPersonen(model);
+        (PersonenQueryResponse personenResponse, List<long>? plIds, List<string>? bsns) = await _gbaService.GetPersonen(model);
 
         personenResponse = await HandleGezagRequest(model, personenResponse, vraagtBsn, bsns);
 
-		AddPlIdsToResponseHeaders(plIds);
+        AddPlIdsToResponseHeaders(plIds);
         return Ok(personenResponse);
-	}
+    }
 
     private async Task<PersonenQueryResponse> HandleGezagRequest(PersonenQuery model, PersonenQueryResponse personenResponse, bool vraagtBsn, List<string>? bsns)
     {
-        if (GezagHelper.GezagIsRequested(model.Fields))
+        if (!GezagHelper.GezagIsRequested(model.Fields)) return personenResponse;
+
+        var personenResponseMetGezag = await _gezagService.GetGezag(personenResponse, model.Fields, bsns);
+        if (!vraagtBsn)
         {
-            bool acceptNewGezag = Request.Headers.ContainsKey("Accept-Gezag-Version");
-
-            if(!acceptNewGezag)
+            if (personenResponseMetGezag is RaadpleegMetBurgerservicenummerResponse response && response != null)
             {
-                var personenResponseMetGezag = await _gezagService.GetGezag(personenResponse, model.Fields, bsns);
-                if (!vraagtBsn)
+                foreach (var p in response.Personen)
                 {
-                    if (personenResponseMetGezag is RaadpleegMetBurgerservicenummerResponse response)
-                    {
-                        if (response != null)
-                        {
-                            foreach (var p in response.Personen)
-                            {
-                                p.Burgerservicenummer = null;
-                            }
-                        }
-                    }
-                    else if (personenResponseMetGezag is ZoekMetAdresseerbaarObjectIdentificatieResponse zoekResponse)
-                    {
-                        if (zoekResponse != null)
-                        {
-                            foreach (var p in zoekResponse.Personen)
-                            {
-                                p.Burgerservicenummer = null;
-                            }
-                        }
-                    }
+                    p.Burgerservicenummer = null;
                 }
-                return personenResponseMetGezag;
-            } 
-            else
-            {
-                var mappedPersonenResponse = ApiModels.BRP.PersonenQueryResponse.MapFrom(personenResponse);
-                var personenResponseMetNewGezag = await _newGezagService.GetGezag(mappedPersonenResponse, model.Fields, bsns);
-                throw new NotImplementedException("New Gezag version is not implemented yet.");
             }
-
+            else if (personenResponseMetGezag is ZoekMetAdresseerbaarObjectIdentificatieResponse zoekResponse && zoekResponse != null)
+            {
+                foreach (var p in zoekResponse.Personen)
+                {
+                    p.Burgerservicenummer = null;
+                }
+            }
         }
-
-        return personenResponse;
+        return personenResponseMetGezag;
     }
 }
