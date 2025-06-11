fa682335c35ddb0f3290a333b886bfd60fd66ad0
Melvin Lee <melvin.lee@iswish.nl>
2025-06-11 11:25:34 +0200

    chore: nswag-gen.sh script aangepast voor draaien op verschillende OS

--- scripts/nswag-gen.sh
+++ scripts/nswag-gen.sh
@@ -1,7 +1,20 @@
 #!/bin/bash
 
-sed -i '' 's/^\([[:space:]]*\)\(properties\)/\1additionalProperties: false\n&/g' ./specificatie/resolved/openapi.yaml
-sed -i '' 's/^\([[:space:]]*\)\(properties\)/\1additionalProperties: false\n&/g' ./specificatie/resolved/openapi-v1.yaml
+# Detect OS and set sed parameters accordingly
+if [[ "$OSTYPE" == "darwin"* ]]; then
+    # macOS
+    SED_INPLACE="-i ''"
+elif [[ "$OSTYPE" == "msys" ]] || [[ "$OSTYPE" == "cygwin" ]] || [[ "$OSTYPE" == "win32" ]]; then
+    # Windows (Git Bash, MSYS2, Cygwin)
+    SED_INPLACE="-i"
+else
+    # Linux and others
+    SED_INPLACE="-i"
+fi
+
+# Apply sed with appropriate flags
+sed $SED_INPLACE 's/^\([[:space:]]*\)\(properties\)/\1additionalProperties: false\n&/g' ./specificatie/resolved/openapi.yaml
+sed $SED_INPLACE 's/^\([[:space:]]*\)\(properties\)/\1additionalProperties: false\n&/g' ./specificatie/resolved/openapi-v1.yaml
 
 npx nswag run src/GezagMock/server.nswag
 npx nswag run src/GezagMock/serverDeprecated.nswag
