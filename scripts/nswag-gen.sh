b501ae1d0a9efa3a421179eb32cf9d5cff638bc5
Melvin Lee <melvin.lee@iswish.nl>
2026-06-04 12:18:24 +0200

    chore: fix sonar issues (#127)

--- scripts/nswag-gen.sh
+++ scripts/nswag-gen.sh
@@ -16,13 +16,13 @@ fi
 sed $SED_INPLACE 's/^\([[:space:]]*\)\(properties\)/\1additionalProperties: false\n&/g' ./specificatie/resolved/openapi.yaml
 sed $SED_INPLACE 's/^\([[:space:]]*\)\(properties\)/\1additionalProperties: false\n&/g' ./specificatie/resolved/openapi-v1.yaml
 
-npx nswag run src/GezagMock/server.nswag
-npx nswag run src/GezagMock/serverDeprecated.nswag
-npx nswag run src/DataTransferObjects.nswag
-npx nswag run src/DataTransferObjectsDeprecated.nswag
-npx nswag run src/DataTransferObjectsCommon.nswag
-npx nswag run src/GezagApiDtos.nswag
-npx nswag run src/GezagApiDtosDeprecated.nswag
+npx --ignore-scripts nswag run src/GezagMock/server.nswag
+npx --ignore-scripts nswag run src/GezagMock/serverDeprecated.nswag
+npx --ignore-scripts nswag run src/DataTransferObjects.nswag
+npx --ignore-scripts nswag run src/DataTransferObjectsDeprecated.nswag
+npx --ignore-scripts nswag run src/DataTransferObjectsCommon.nswag
+npx --ignore-scripts nswag run src/GezagApiDtos.nswag
+npx --ignore-scripts nswag run src/GezagApiDtosDeprecated.nswag
 
 git checkout -- ./specificatie/resolved/openapi.yaml
 git checkout -- ./specificatie/resolved/openapi-v1.yaml
--- src/Rvig.HaalCentraalApi.Personen/Helpers/GezagHelper.cs
+++ src/Rvig.HaalCentraalApi.Personen/Helpers/GezagHelper.cs
@@ -15,43 +15,73 @@ namespace Rvig.HaalCentraalApi.Personen.Helpers
 
             foreach (var x in gezagsrelaties)
             {
-                if (x is TweehoofdigOuderlijkGezag y)
-                {
-                    gezagBsns.AddRange(from z in y.Ouders
-                                       select z.Burgerservicenummer);
-                    gezagBsns.Add(y.Minderjarige.Burgerservicenummer);
-                }
-                if (x is EenhoofdigOuderlijkGezag y2)
-                {
-                    gezagBsns.Add(y2.Ouder.Burgerservicenummer);
-                    gezagBsns.Add(y2.Minderjarige.Burgerservicenummer);
-                }
-                if (x is Voogdij v)
-                {
-                    gezagBsns.AddRange(from z in v.Derden
-                                       select z.Burgerservicenummer);
-                    gezagBsns.Add(v.Minderjarige.Burgerservicenummer);
-                }
-                if (x is GezamenlijkGezag gz)
-                {
-                    if(gz.Derde is BekendeDerde bekendeDerde)
-                    {
-                        gezagBsns.Add(bekendeDerde.Burgerservicenummer);
-                    }
-                    gezagBsns.Add(gz.Ouder.Burgerservicenummer);
-                    gezagBsns.Add(gz.Minderjarige.Burgerservicenummer);
-                }
-                if (x is GezagNietTeBepalen gn && gn.Minderjarige != null)
-                {
-                    gezagBsns.Add(gn.Minderjarige.Burgerservicenummer);
-                }
-                if (x is TijdelijkGeenGezag tg && tg.Minderjarige != null)
-                {
-                    gezagBsns.Add(tg.Minderjarige.Burgerservicenummer);
-                }
+                GetTweehoofdigOuderlijkGezagBsns(gezagBsns, x);
+                GetEenhoofdigOuderlijkGezagBsns(gezagBsns, x);
+                GetVoogdijBsns(gezagBsns, x);
+                GetGezamenlijkGezagBsns(gezagBsns, x);
+                GetGezagNietTeBepalenBsns(gezagBsns, x);
+                GetTijdelijkGeenGezagBsns(gezagBsns, x);
             }
 
             return gezagBsns.Distinct().ToList();
+        }
+
+        private static void GetTijdelijkGeenGezagBsns(List<string> gezagBsns, AbstractGezagsrelatie x)
+        {
+            if (x is TijdelijkGeenGezag tg && tg.Minderjarige != null)
+            {
+                gezagBsns.Add(tg.Minderjarige.Burgerservicenummer);
+            }
+        }
+
+        private static void GetGezagNietTeBepalenBsns(List<string> gezagBsns, AbstractGezagsrelatie x)
+        {
+            if (x is GezagNietTeBepalen gn && gn.Minderjarige != null)
+            {
+                gezagBsns.Add(gn.Minderjarige.Burgerservicenummer);
+            }
+        }
+
+        private static void GetGezamenlijkGezagBsns(List<string> gezagBsns, AbstractGezagsrelatie x)
+        {
+            if (x is GezamenlijkGezag gz)
+            {
+                if (gz.Derde is BekendeDerde bekendeDerde)
+                {
+                    gezagBsns.Add(bekendeDerde.Burgerservicenummer);
+                }
+                gezagBsns.Add(gz.Ouder.Burgerservicenummer);
+                gezagBsns.Add(gz.Minderjarige.Burgerservicenummer);
+            }
+        }
+
+        private static void GetVoogdijBsns(List<string> gezagBsns, AbstractGezagsrelatie x)
+        {
+            if (x is Voogdij v)
+            {
+                gezagBsns.AddRange(from z in v.Derden
+                                   select z.Burgerservicenummer);
+                gezagBsns.Add(v.Minderjarige.Burgerservicenummer);
+            }
+        }
+
+        private static void GetEenhoofdigOuderlijkGezagBsns(List<string> gezagBsns, AbstractGezagsrelatie x)
+        {
+            if (x is EenhoofdigOuderlijkGezag y2)
+            {
+                gezagBsns.Add(y2.Ouder.Burgerservicenummer);
+                gezagBsns.Add(y2.Minderjarige.Burgerservicenummer);
+            }
+        }
+
+        private static void GetTweehoofdigOuderlijkGezagBsns(List<string> gezagBsns, AbstractGezagsrelatie x)
+        {
+            if (x is TweehoofdigOuderlijkGezag y)
+            {
+                gezagBsns.AddRange(from z in y.Ouders
+                                   select z.Burgerservicenummer);
+                gezagBsns.Add(y.Minderjarige.Burgerservicenummer);
+            }
         }
     }
 }
